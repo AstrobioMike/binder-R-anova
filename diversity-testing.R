c17bff670aa8261eb3b24ca3b7d20eb75cd9686c
AstrobioMike <michael.lee0517@gmail.com>
2021-05-12 23:23:21 -0700

    update

--- diversity-testing.R
+++ diversity-testing.R
@@ -8,17 +8,20 @@ library(tidyverse)
 library(magrittr)
 
 # making helper functions to run divnet and testing following along with tutorial here: https://github.com/adw96/DivNet/blob/master/vignettes/getting-started.Rmd)
-# helper functions for mass running things
+
 make_div_obj <- function(phy_obj, wanted_formula) {
 
     # running divnet
-    div_obj <- phy_obj %>% divnet(formula = wanted_formula, ncores = 4)
+    div_obj <- phy_obj %>% divnet(formula = wanted_formula)
 
     return(div_obj)
 
 }
 
-run_div_test <- function(phy_obj, div_obj, target_covariate, target_ref = NULL) {
+run_div_test <- function(phy_obj, div_obj, target_ref = NULL) {
+
+    # getting covariate
+    target_covariate <- attributes(div_obj$X)$contrasts %>% names
 
     # running testing
     estimates <- div_obj$shannon %>% summary %$% estimate
@@ -39,60 +42,75 @@ run_div_test <- function(phy_obj, div_obj, target_covariate, target_ref = NULL) 
 
     res <- betta(estimates, ses, X)
 
-    # global p-value (are any of the groups different)
-    global_p <- res$global[2]
-
-    cat("Global p-value: ", global_p, "\n\n", sep = "")
-
     cat("With '", baseline, "' as the baseline:\n\n", sep = "")
 
-    print(res$table)
+    tab <- res$table %>% data.frame
+
+    # p-values are calculated as: 2*(1-pnorm(abs(Estimate/SE))) (see betta function)
+    # I think getting 0s is just due to how many digits R is working with. It seems
+    # pnorm(5.32672)  = 0.9999999 and pnorm(5.32673) = 1. So if our abs(Estimate/SE)
+    # is greater than 5.32672, we’re going to get a p-value of 0. So setting 0's to
+    # < 1e-8 for what is printed out
+
+    new_p_vec <- vector()
+
+    for ( value in tab$p.values ) {
+        if ( value == 0 ) {
+            new_p_vec <- c(new_p_vec, "< 1e-8")
+        } else {
+            new_p_vec <- c(new_p_vec, value)
+        }
+    }
+
+    tab$p.values <- new_p_vec
+
+    print(tab)
 
     return(res)
 
 }
 
-# doing a test with the data stored with the divnet package and used in the tutorial linked above - it's called Lee in the package because it comes from me :)
-data(Lee)
+plot_DivNet_diversity <- function(div_obj, sample_tab = sample_info_tab) {
 
-# collapsing at phylum level for testing
-lee_phylum <- tax_glom(Lee, taxrank = "Phylum")
-lee_phylum %>% sample_data
-# making divnet object based on "char" column in sample data
-test_div_obj <- make_div_obj(lee_phylum, ~char)
+    # getting covariate
+    target_covariate <- attributes(div_obj$X)$contrasts %>% names
 
-# running test based on "characteristic" in the test data
-test_div_res <- run_div_test(lee_phylum, test_div_obj, "char")
-# Global p-value: 0
-#
-# With 'altered' as the baseline:
-#
-#                        Estimates Standard Errors p-values
-# (Intercept)           1.34484669      0.01007056    0.000
-# predictors_biofilm   -0.91111064      0.11083815    0.000
-# predictors_carbonate -0.05379851      0.13020724    0.679
-# predictors_glassy    -0.20930449      0.02420628    0.000
-# predictors_water      0.11472333      0.09311783    0.218
+    # getting unique treatment types
+    unique_treatment_types <- sample_tab %>% pull(target_covariate) %>% unique %>% as.character
 
-    # good, works like it's supposed to, global diff p-value is 0 saying something is diff
-    # 'altered' is the baseline and has greater shannon diversity than glassy.
-    # run as above, we are only comparing everything against 'altered'
-    # here's how we can run it to compare everything against something else, e.g. 'water'
+    unique_samples_needed <- c()
 
-test_div_res2 <- run_div_test(lee_phylum, test_div_obj, "char", target_ref = "water")
-# Global p-value: 0
-#
-# With 'water' as the baseline:
-#
-#                       Estimates Standard Errors p-values
-# (Intercept)           1.4595700      0.01007056    0.000
-# predictors_altered   -0.1147233      0.01125231    0.000
-# predictors_biofilm   -1.0258340      0.11083815    0.000
-# predictors_carbonate -0.1685218      0.13020724    0.196
-# predictors_glassy    -0.3240278      0.02420628    0.000
+    for ( type in unique_treatment_types ) {
 
-    # and that shows us everything is diff from the water (and lower in diversity), except the carbonate sample
-    # okay, helper functions check out based on what the test data set looks like, now moving onto our stuff
+        sample_to_add <- sample_tab %>% filter(!!as.symbol(target_covariate) == type) %>% head(1) %>% row.names()
+
+        unique_samples_needed <- c(unique_samples_needed, sample_to_add)
+    }
+
+    sub_div_obj_shannon <- div_obj$shannon[c(unique_samples_needed)]
+
+    div_est_vec <- c()
+    ci_vec <- c()
+
+    for ( i in 1:length(unique_samples_needed) ) {
+        div_est_vec <- c(div_est_vec, unlist(sub_div_obj_shannon[i])[1] %>% as.numeric())
+        ci_vec <- c(ci_vec, unlist(sub_div_obj_shannon[i])[2] %>% as.numeric() * 2)
+    }
+
+    sub_plot_tab <- data.frame(target_covariate = unique_treatment_types, shannon = div_est_vec, ci = ci_vec)
+
+    # getting max value for plot y-axis
+    max_y <- sub_plot_tab$shannon %>% max %>% ceiling
+
+    plot <- ggplot(sub_plot_tab, aes(x = target_covariate, y = shannon, color = target_covariate)) +
+        geom_errorbar(aes(ymin = shannon - ci, ymax = shannon + ci), width = 0) +
+        geom_point() + theme_bw() +
+        xlab(target_covariate) + ylab("Shannon diversity estimate\n(Class level)") +
+        theme(legend.position = "none") + coord_cartesian(ylim = c(0,max_y))
+
+    return(plot)
+
+}
 
 
 # reading in our data (excluding EM1 and Lambda)
@@ -108,67 +126,126 @@ aqua_phy <- phyloseq(counts_phy, tax_phy, sample_phy)
 
 # grouping at diff ranks
 aqua_class_phy <- tax_glom(aqua_phy, taxrank = "Class")
-aqua_order_phy <- tax_glom(aqua_phy, taxrank = "Order")
-aqua_family_phy <- tax_glom(aqua_phy, taxrank = "Family")
+# aqua_order_phy <- tax_glom(aqua_phy, taxrank = "Order")
+# aqua_family_phy <- tax_glom(aqua_phy, taxrank = "Family")
 
-    # factors we'll look at
+    # factors we'll look at (we'll need these specific names exactly as used below to tell the programs what to look at)
 names(sample_info_tab)
 
-## starting at CLASS level
-    # Time_point
+    ## Time_point
+
+        # first function creates the DivNet object holding the estimated diversities
+        # we need to give it the phyloseq object, that's the first argument here
+        # and the second starts with the tilde '~', which means "as a function of", and we then give it the column name from our sample info table, here doing 'Time_point'
 class_time_point_div_obj <- make_div_obj(aqua_class_phy, ~Time_point)
-class_time_point_T1_base_div_res <- run_div_test(aqua_class_phy, class_time_point_div_obj, "Time_point")
-# Global p-value: 0
-#
+
+        # this second function does the actual hypothesis test of whether there is a difference or not
+        # we need to give it the same phyloseq object (first argument)
+        # then the DivNetobject we just made (second argument)
+        # then last we need to pass a string this time (meaning in quotes), that specifies what we're testing, the same "Time_point" column we specified above (just needs to be done with quotes this time and not before)
+class_time_point_T1_base_div_res <- run_div_test(aqua_class_phy, class_time_point_div_obj)
+
 # With 'T1' as the baseline:
-#
-#                 Estimates Standard Errors p-values
-# (Intercept)    1.48577272      0.01873717    0.000
-# predictors_T2  0.12386158      0.02641508    0.000
-# predictors_T3 -0.06554238      0.04737824    0.167
+
+#                 Estimates Standard.Errors p.values
+# (Intercept)    1.48609385      0.01642516   < 1e-8
+# predictors_T2  0.12242525      0.04933613    0.013
+# predictors_T3 -0.06621529      0.02088762    0.002
 
     # Intercept is T1, as that's the only one not listed
-    # this says T1 was sig. diff from T2 (p-value), and T2 had a higher diversity (estimate is positive 0.1238...)
+    # this says T1 was sig. diff from T2 (p-value of 0.013), and T2 had a higher diversity (estimate is positive 0.122...)
+    # and says T3 was sig. diff than T1, lower diversity (This changed a little since last time as I modified how we are running the code)
+
     # let's look with T2 as the base, this will tell us if T2 was sig diff from T3
 class_time_point_T2_base_div_res <- run_div_test(aqua_class_phy, class_time_point_div_obj, "Time_point", target_ref = "T2")
-# Global p-value: 0
-#
+
 # With 'T2' as the baseline:
-#
-#                Estimates Standard Errors p-values
-# (Intercept)    1.6096343      0.01873717        0
-# predictors_T1 -0.1238616      0.03211331        0
-# predictors_T3 -0.1894040      0.04737824        0
+# 
+#                Estimates Standard.Errors p.values
+# (Intercept)    1.6085191      0.01642516   < 1e-8
+# predictors_T1 -0.1224252      0.03156335   < 1e-8
+# predictors_T3 -0.1886405      0.02088762   < 1e-8
+
 
     # this says T2 was sig. diff from T3 (says T1 too, but we already knew that one from above)
-    # T3 is 0.1894... lower in estimated shannon diversity than T2, with a p-value < 0.000
+    # T3 is 0.188... lower in estimated shannon diversity than T2, with a p-value < 1e-8
 
     ## so based on time point, T2 has the highest diversity, T2 is sig higher than T1, and T3 is sig lower than T2, T3 is not sig higher than T1
-    # let's see if we can plot these with their error bars
-
-class_time_point_div_obj$shannon %>%
-    plot(aqua_class_phy, color = "Time_point") +
-    xlab("Time point") +
-    ylab("Shannon diversity estimate\n(Class level)")
-
-   # since the estimate is based on all of them, they are all the same, so only going to plot one per Time point, rather than all
-    # i'm sure there is a better way to do this, but now isn't the time for figuring out better ways, it's the time for doing things, ha
-sub_time_point_div_obj_shannon <- class_time_point_div_obj$shannon[c(1,11,21)]
-names(sub_time_point_div_obj_shannon) <- c("T1", "T2", "T3")
-
-div_est_vec <- c(unlist(sub_time_point_div_obj_shannon[1])[1] %>% as.numeric(), unlist(sub_time_point_div_obj_shannon[2])[1] %>% as.numeric(), unlist(sub_time_point_div_obj_shannon[3])[1] %>% as.numeric())
-ci_vec <- c(unlist(sub_time_point_div_obj_shannon[1])[2] %>% as.numeric() * 2, unlist(sub_time_point_div_obj_shannon[2])[2] %>% as.numeric() * 2, unlist(sub_time_point_div_obj_shannon[3])[2] %>% as.numeric() * 2)
-sub_time_plot_tab <- data.frame(Time_point = names(sub_time_point_div_obj_shannon), shannon = div_est_vec, ci = ci_vec)
-
-ggplot(sub_time_plot_tab, aes(x = Time_point, y = shannon, color = Time_point)) +
-    geom_errorbar(aes(ymin = shannon - ci, ymax = shannon + ci), width = 0) +
-    geom_point() + theme_bw() + coord_cartesian(ylim = c(0,2)) +
-    xlab("Time point") + ylab("Shannon diversity estimate\n(Class level)") +
-    theme(legend.position = "none")
+    # we can plot these with confidence intervals with this helper function now :)
+plot_DivNet_diversity(class_time_point_div_obj)
 
 
-## will look at others too, but haven't yet, e.g. others we have in our sample table
+## now looking at lighting
 names(sample_info_tab)
 
-  # and can look at other rank summarizations, meaning other than class, probably shouldn't go lower than family though, and we staying higher is generally better because of the high error rate in nanopore sequencing
+## lighting
+
+class_lighting_div_obj <- make_div_obj(aqua_class_phy, ~Lighting)
+class_lighting_div_res <- run_div_test(aqua_class_phy, class_lighting_div_obj)
+
+#                    Estimates Standard.Errors p.values
+# (Intercept)         1.420162      0.01193468   < 1e-8
+# predictors_Natural  0.136010      0.02141959   < 1e-8
+
+    # says natural has greater diversity 
+
+# plotting
+plot_DivNet_diversity(class_lighting_div_obj)
   
+    # looking at counts of plants in each lighting type
+sample_info_tab %>% group_by(Lighting, Plants) %>% summarize(n = n())
+#   Lighting   Plants       n
+#   <fct>      <fct>    <int>
+# 1 Artificial Basil        6
+# 2 Artificial Peas         6
+# 3 Artificial Tomatoes     3
+# 4 Natural    Basil        6
+# 5 Natural    Peas         3
+# 6 Natural    Tomatoes     6
+
+
+## plants
+class_plants_div_obj <- make_div_obj(aqua_class_phy, ~Plants)
+class_plants_div_res <- run_div_test(aqua_class_phy, class_plants_div_obj)
+
+# With 'Basil' as the baseline:
+
+#                     Estimates Standard.Errors p.values
+# (Intercept)         1.3583161      0.01947881   < 1e-8
+# predictors_Peas     0.1378274      0.04539046    0.002
+# predictors_Tomatoes 0.2520203      0.03179659   < 1e-8
+
+# running one with Peas as baseline so we can see if peas and tomatoes are diff
+class_plants_div_res_peas_baseline <- run_div_test(aqua_class_phy, class_plants_div_obj, target_ref = "Peas")
+# With 'Peas' as the baseline:
+# 
+#                      Estimates Standard.Errors p.values
+# (Intercept)          1.4961435      0.01947881   < 1e-8
+# predictors_Basil    -0.1378274      0.02934699   < 1e-8
+# predictors_Tomatoes  0.1141929      0.03179659   < 1e-8
+
+plot_DivNet_diversity(class_plants_div_obj)
+
+#### i think i need to double check with Amy (developer of DivNet) that there isn't a problem with the tutorial i'm following, because this seems a little off to me that they are almost all significant
+
+    ## for now, let's finish figuring out how we can split the phyloseq object based on plant type
+
+#### splitting into plant-specific phyloseq objects
+aqua_class_phy
+    # ah, wonderfully there is already a function built for this :)
+aqua_class_peas_phy <- subset_samples(aqua_class_phy, Plants == "Peas")
+aqua_class_basil_phy <- subset_samples(aqua_class_phy, Plants == "Basil")
+aqua_class_tomatoes_phy <- subset_samples(aqua_class_phy, Plants == "Tomatoes")
+
+    # looking at timepoint of just the peas
+class_timepoints_peas_div_obj <- make_div_obj(aqua_class_peas_phy, ~Time_point)
+class_timepoints_peas_div_res <- run_div_test(aqua_class_peas_phy, class_timepoints_peas_div_obj)
+    # we need to specify a subset sample table too for this plotting function to work properly (that's the second argument here now)
+plot_DivNet_diversity(class_timepoints_peas_div_obj, sample_tab = data.frame(sample_data(aqua_class_peas_phy)))
+
+    # just the basil
+class_timepoints_basil_div_obj <- make_div_obj(aqua_class_basil_phy, ~Time_point)
+class_timepoints_basil_div_res <- run_div_test(aqua_class_basil_phy, class_timepoints_basil_div_obj)
+plot_DivNet_diversity(class_timepoints_basil_div_obj, sample_tab = data.frame(sample_data(aqua_class_basil_phy)))
+
+
